04f0bdf7d2b17592d79cea7217ae133b26015b2c
Noah Alexandroff <noahalexandroff@gmail.com>
2022-03-18 18:05:45 -0400

    function to extract sol from mer filenames

--- src/scrape_mer_depth_data.py
+++ src/scrape_mer_depth_data.py
@@ -10,3 +10,21 @@ from PIL import Image
 from planetaryimage import PDS3Image
   
 import argparse
+
+
+def mer_extract_sol(filename):
+    """
+    Extracts the sol from the filename for mer data
+
+    Parameters:
+    -----------
+    filename: the name of the mer file that we are trying to find the sol of
+
+    Returns:
+    --------
+    sol: the sol that corresponds to the provided filename
+    """
+    sclk = filename[4:13]
+    sol = math.floor((float(sclk)-128141132.)/88775.42064)
+    return sol
+
