20fd790dc21b20722c884afc6c2990e6cb581345
Noah Alexandroff <noahalexandroff@gmail.com>
2022-03-17 22:59:19 -0400

    function to generate a url tha can be used to download from PDS

--- src/scrape_msl_depth_data.py
+++ src/scrape_msl_depth_data.py
@@ -28,4 +28,36 @@ def msl_extract_sol(filename):
     sol = math.floor((float(sclk)-397446468.)/88775.42064)
     return sol
     
+def msl_create_url(filename, prodid = "RNG", extension='.IMG'):
+    """
+    Generates a url that can be used to download msl files from the PDS. Default values will
+    return url for RNG depth data.
+
+    Parameters:
+    -----------
+    filename: the name of the MSL EDR file contained in AI4Mars dataset
+
+    prodid: the prodid of the desired url. this determines which type of data will be  
+    downloaded. the default value is "RNG". more information about available prodids at
+    https://pds-imaging.jpl.nasa.gov/data/msl/MSLNAV_1XXX/DOCUMENT/MSL_CAMERA_SIS.PDF
+
+
+    extension: the extension to be used for the desired url. the default value is ".IMG"
+
+
+    Returns:
+    --------
+    combined_url : the generated url that can be used to download files from the PDS
+    """
+
+    sol = msl_extract_sol(filename)
+    base_url = "https://pds-imaging.jpl.nasa.gov/data/msl/MSLNAV_1XXX" 
+    if sol<1870: #Changed location of data at sol 1870
+        data_url = "/DATA_V1/"
+    else:
+        data_url = "/DATA/"
+    sol_url = "SOL" + f'{sol:05d}'+"/"
+    file_url = update_filename_from_edr(filename, prodid = prodid, extension=extension)
+    combined_url = base_url + data_url + sol_url + file_url
     
+    return combined_url
