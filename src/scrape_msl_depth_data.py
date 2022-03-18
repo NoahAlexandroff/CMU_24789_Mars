01c3d8b2f3739c2844600bbc4b93823f6e9e92a7
Noah Alexandroff <noahalexandroff@gmail.com>
2022-03-17 22:55:27 -0400

    added function to extract the sol from filename

--- src/scrape_msl_depth_data.py
+++ src/scrape_msl_depth_data.py
@@ -10,3 +10,22 @@ from PIL import Image
 from planetaryimage import PDS3Image
   
 import argparse
+
+
+def msl_extract_sol(filename):
+    """
+    Extracts the sol from the filename for msl data
+
+    Parameters:
+    -----------
+    filename: the name of the msl file that we are trying to find the sol of
+
+    Returns:
+    --------
+    sol: the sol that corresponds to the provided filename
+    """
+    sclk = filename[4:13]
+    sol = math.floor((float(sclk)-397446468.)/88775.42064)
+    return sol
+    
+    
