dd351a031c799a822cc88c26411d1871c9f86186
Noah Alexandroff <noahalexandroff@gmail.com>
2022-03-18 16:25:04 -0400

    shell script to run multiple scraping scripts at once

---
+++ shell_scripts/scrape_msl.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+start=0
+end=6500
+skip=500
+
+for start_ind in {$start..$(($end-$skip))..$skip}
+    do
+        end_ind=$(($start_ind+$skip))
+        python src/scrape_msl_depth_data.py --starting-index $start_ind --ending-index $end_ind &
+done
