125f359c436e8f12369ae7d493ace8bec55ea6c6
Noah Alexandroff <noahalexandroff@gmail.com>
2022-03-19 16:48:34 -0400

    function to create new url from existing files

--- src/scrape_mer_depth_data.py
+++ src/scrape_mer_depth_data.py
@@ -32,3 +32,37 @@ def mer_extract_sol(filename):
     
     return sol
 
+def mer_create_url(filename, prodid = "rnl", extension='.img'):
+    """
+    Generates a url that can be used to download mer files from the PDS. Default values will
+    return url for rnl depth data.
+
+    Parameters:
+    -----------
+    filename: the name of the MER eff file contained in AI4Mars dataset
+
+    prodid: the prodid of the desired url. this determines which type of data will be  
+    downloaded. the default value is "rnl". more information about available prodids at either
+    https://pds-imaging.jpl.nasa.gov/data/mer/spirit/mer2no_0xxx/document/CAMSIS_V4-4_7-31-14.PDF or
+    https://pds-imaging.jpl.nasa.gov/data/mer/opportunity/mer1no_0xxx/document/CAMSIS_V4-4_7-31-14.PDF
+    depending upon the desired rover
+
+    extension: the extension to be used for the desired url. the default value is ".img"
+
+
+    Returns:
+    --------
+    combined_url : the generated url that can be used to download files from the PDS
+    """
+
+    sol = msl_extract_sol(filename)
+    base_url = "https://pds-imaging.jpl.nasa.gov/data/mer/"
+    if filename[0]=='1':
+        data_url = "opportunity/mer1no_0xxx/data/"
+    else: 
+        data_url = "spirit/mer2no_0xxx/data/"
+    sol_url = "sol" + f'{sol:05d}'+"/rdr/"
+    file_url = update_filename_from_edr(filename, prodid = prodid, extension=extension)
+    combined_url = base_url + data_url + sol_url + file_url
+    
+    return combined_url
