34e2daa626d757fc6053a305b061c6091cffe83b
Noah Alexandroff <noahalexandroff@gmail.com>
2022-04-30 22:37:44 -0400

    updated loss function to be dice loss

--- unet/train_unet.py
+++ unet/train_unet.py
@@ -1,14 +1,16 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 from ai4mars_dataset import AI4MarsDataset
 from unet import UNet
 import pickle
+from unet_loss import dice_loss
 
 
-def main(num_epochs=5, batch_size=16, dataroot="./data_subset/msl/", image_size = 256):
+
+def main(num_epochs=5, batch_size=2, dataroot="./data_subset/msl/", image_size = 256):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if device == torch.device('cpu'):
         num_workers = 0
@@ -32,17 +34,17 @@ def main(num_epochs=5, batch_size=16, dataroot="./data_subset/msl/", image_size 
                                              shuffle=False, num_workers=num_workers)
 
     model = UNet(in_channels=1, n_classes=5)
-    record = [[], [], []]
+    record = [[], [], [], []]
 
 
-    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters())
     model.to(device)
     best_model_test_acc = 0
     
     for epoch in range(num_epochs):  # loop over the dataset multiple timesi
         
-        running_loss = 0.0
+        training_loss, test_loss = 0.0, 0.0
         train_total, test_total = 0, 0
         train_correct, test_correct = 0, 0
         model.train()
@@ -62,12 +64,13 @@ def main(num_epochs=5, batch_size=16, dataroot="./data_subset/msl/", image_size 
             
             outputs = model(images.to(device))
             
-            loss = criterion(outputs, labels.to(device))
+            # loss = criterion(outputs, labels.to(device))
+            loss = dice_loss(labels.to(device),outputs)
             loss.backward()
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
+            training_loss += loss.item()
             seg_acc = ((labels.cpu() == torch.argmax(outputs, axis=1).cpu()).sum() / torch.numel(labels.cpu())).item()
             print('batch_acc:',seg_acc)
             _, train_predict = torch.max(outputs.data, 1)
@@ -85,15 +88,19 @@ def main(num_epochs=5, batch_size=16, dataroot="./data_subset/msl/", image_size 
                 labels[labels==255]=4
                 outputs = model(images.to(device))
                 _, test_predict = torch.max(outputs.data, 1)
+                loss = dice_loss(labels.to(device),outputs)
                 test_total += labels.size(0)
+                test_loss += loss.item()
                 test_correct += (test_predict == labels.to(device)).sum().item()
         train_acc = train_correct/train_total
         test_acc = test_correct/test_total
         record[0].append(train_acc)
         record[1].append(test_acc)
-        record[2].append(running_loss/len(trainloader))
+        record[2].append(training_loss/len(trainloader))
+        record[3].append(test_loss/len(testloader))
+
         print('Epoch %d| Train loss: %.4f| Train Acc: %.3f| Test Acc: %.3f'%(
-            epoch+1, running_loss/len(trainloader), train_acc, test_acc))
+            epoch+1, training_loss/len(trainloader), test_loss/len(testloader) , train_acc, test_acc))
         if test_acc>best_model_test_acc:
             best_model_test_acc=test_acc
             
