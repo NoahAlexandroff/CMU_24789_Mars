c73c1849e16f1e85c7083f28bbfcc54fb58ae78f
Noah Alexandroff <noahalexandroff@gmail.com>
2022-05-09 21:16:43 -0400

    sameer's loss function for his training notebook

---
+++ unet/unet_loss_MaskedL2.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def gradient(x):
+    # idea from tf.image.image_gradients(image)
+    # https://github.com/tensorflow/tensorflow/blob/r2.1/tensorflow/python/ops/image_ops_impl.py#L3441-L3512
+    # x: (b,c,h,w), float32 or float64
+    # dx, dy: (b,c,h,w)
+
+    h_x = x.size()[-2]
+    w_x = x.size()[-1]
+    # gradient step=1
+    left = x
+    right = F.pad(x, [0, 1, 0, 0])[:, :, :, 1:]
+    top = x
+    bottom = F.pad(x, [0, 0, 0, 1])[:, :, 1:, :]
+
+    # dx, dy = torch.abs(right - left), torch.abs(bottom - top)
+    dx, dy = right - left, bottom - top
+    # dx will always have zeros in the last column, right-left
+    # dy will always have zeros in the last row,    bottom-top
+    dx[:, :, :, -1] = 0
+    dy[:, :, -1, :] = 0
+
+    return dx, dy
+def MaskedL2(true, logits):
+    valid_mask = (logits > 0).detach()
+
+    diffs = logits - true
+    diffs = diffs[valid_mask]
+
+    loss = torch.mean(torch.pow(diffs, 2))
+
+    return loss
+
+
+def combined_loss(true, logits):
+    loss = MaskedL2(true, logits)
+
+
+    return loss
